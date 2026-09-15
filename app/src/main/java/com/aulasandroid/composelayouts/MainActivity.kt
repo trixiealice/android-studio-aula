28b2dbd08b875360cec19d19c05b5e34c9e31592
ALICE CAMPOS DA SILVA <25203690@dsn127m1247443.cfp127aluno.local>
2026-09-15 15:34:47 -0300

    a

--- app/src/main/java/com/aulasandroid/composelayouts/MainActivity.kt
+++ app/src/main/java/com/aulasandroid/composelayouts/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aulasandroid.composelayouts.ui.theme.ComposeLayoutsTheme
@@ -19,32 +27,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent {1
             ComposeLayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Text("Aula Android")
+                    LayoutScreen(modifier = Modifier.padding(innerPadding))
+
                     }
                 }
             }
         }
     }
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LayoutScreen(modifier: Modifier = Modifier) {
+    Column( modifier = modifier.background(Color.Cyan)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeLayoutsTheme {
-        Greeting("Android")
+    ) {
+        Column (
+            modifier = Modifier.background(Color.Yellow)
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Texto 4")
+            Text("Texto 5")
+            Text("Texto 6")
+
+
+        }
+        Row(
+            modifier = Modifier.background(Color.Red)
+                .weight(1f)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ) {
+            Text("Texto 7")
+            Text("Texto 8")
+            Text("Texto 9")
+        }
     }
 }
